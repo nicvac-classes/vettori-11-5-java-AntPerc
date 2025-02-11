b658587c63f934933bc943682f19fbcf5c223403
AntPerc <antonio.percuoco@marconibari.edu.it>
2025-02-11 16:12:09 +0000

    esercizio 11.5 svolto

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,49 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+import java.util.scanner;
 
-//Import di Classi Java necessarie al funzionamento del programma
-import java.util.Scanner;
+public class Esercizio {
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    public static void main(String[] args) {
+        int n, coppie, cornici, i, j, k;
+        boolean trovato;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        System.out.println("Inserisci il numero di listelli: ");
+        n = input.nextInt();
+        int[] listelli = new int[n];
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        for (i = 0; i <= n - 1; i++) {
+            listelli[i] = random.nextInt(7) + 4;
+            System.out.println(listelli[i]);
+        }
+        trovato = false;
+        coppie = 0;
+        while (n >= 2 && trovato) {
+            trovato = false;
+            for (i = 0; i <= n - 2; i++) {
+                for (j = i + 1; j <= n - 1; j++) {
+                    if (listelli[i] == listelli[j]) {
+                        n = rimuoviElemento(listelli, n, i);
+                        n = rimuoviElemento(listelli, n, j);
+                        trovato = true;
+                        coppie = coppie + 1;
+                        i = 0;
+                    }
+                }
+            }
+        }
+        cornici = (double) coppie / 2;
+        System.out.println("Ha fabbricato " + cornici + " cornici");
+    }
+    
+    public static int rimuoviElemento(int[] vettore, int dimensione, int posizione) {
+        int i;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        dimensione = dimensione - 1;
+        for (i = posizione; i <= dimensione - 1; i++) {
+            vettore[i] = vettore[i + 1];
+        }
+        
+        return dimensione;
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
